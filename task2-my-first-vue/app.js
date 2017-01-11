fc24428668f375a3b87ef39b3dfa2392b4cf1e1e
HJN <HJN>
2017-01-11 23:56:00 +0800

    task2

--- task2-my-first-vue/app.js
+++ task2-my-first-vue/app.js
@@ -1,3 +1,18 @@
-import bar from './bar';
+import bar from './bar'
+import Vue from 'vue'
 
-bar()
+var app1 = new Vue({
+  el: '#app1',
+  data: {
+    message1: '今天红茶有点喝多了',
+    message2: '半夜睡不着就写代码吧'
+  }
+})
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    message1: '为什么从咖啡换成红茶了呢',
+    message2: '因为咖啡喝不起了',
+    message3: '就连便宜的G7也买不起了'
+  }
+})
