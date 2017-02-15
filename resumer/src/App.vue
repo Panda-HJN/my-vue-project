3585fcdd28f17b296435ef432f0e76490b8dfdad
HJN <HJN>
2017-02-15 13:13:07 +0800

    resumer

---
+++ resumer/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class=page>
+   <header>
+      <Topbar/>
+  </header>
+   <main>
+      <ResumeEditor/>
+     <ResumePreview/>
+   </main>
+</div>
+</template>
+
+<script>
+    import 'normalize.css/normalize.css'
+    import './assets/reset.css'
+    import Topbar from './components/Topbar'
+    import ResumeEditor from './components/ResumeEditor'
+    import ResumePreview from './components/ResumePreview'
+
+    export default {
+        name: 'app',
+        data: function() {
+            return {
+                text: '你好'
+            }
+        },
+        components: {
+            Topbar,
+            ResumeEditor,
+            ResumePreview
+        }
+    }
+</script>
+
+<style>
+    .page {
+        height: 100vh;
+        background: #EAEBEC;
+        display: flex;
+        flex-direction: column;
+    }
+    
+    .page>main {
+        flex-grow: 1;
+    }
+    
+    .page>main {
+        min-width: 1024px;
+        max-width: 1440px;
+        margin: 0;
+        margin-top: 16px;
+        margin-bottom: 16px;
+        width: 100%;
+        align-self: center;
+        display: flex;
+        justify-content: space-around;
+        justify-content: space-between;
+        padding: 0 16px;
+    }
+    
+    #resumeEditor {
+        width: 35%;
+        background: #444;
+    }
+    
+    #resumePreview {
+        width: 61.66667%;
+        background: #777;
+        flex-grow: 1;
+        margin-left: 16px;
+    }
+</style>
